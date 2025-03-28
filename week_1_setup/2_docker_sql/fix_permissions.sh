f8221c1b4ed7c4dba42e5deac5090d82b37e0dfe
GuilleAlte <92647260+GuilleAlte@users.noreply.github.com>
2025-03-28 12:44:52 +0000

    postgres commit

---
+++ week_1_setup/2_docker_sql/fix_permissions.sh
@@ -0,0 +1,3 @@
+echo '#!/bin/bash' > fix_permissions.sh
+echo 'sudo chmod -R 744 /workspaces/Zoomcamp2025/week_1_setup/2_docker_sql/ny_taxi_postgres_data' >> fix_permissions.sh
+chmod +x fix_permissions.sh
